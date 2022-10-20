a1503f21be9494d6edb4759ca04c47d693dba68a
mohee <mohee@DESKTOP-P54N9I6>
2022-10-20 13:55:57 +0530

    first commit

---
+++ B28/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mohee
+ *
+ */
+module B28 {
+}
---
+++ B28/src/org/tns/program/Firstprogram.java
@@ -0,0 +1,25 @@
+package org.tns.program;
+
+public class Firstprogram {
+
+	int year_of_purchase;//data members
+	String make;
+	int model;
+	double cost;
+	public void display()//member function
+	{
+		System.out.println("Year of Purchase "+2023);
+		System.out.println("Make "+"Himalaiyan 411 ");
+	}
+	void display2()
+	{
+		System.out.println("Model "+ 2016);
+		System.out.println("Make "+275000);
+	}
+    public static void main (String [] args)
+    {
+    Firstprogram ob1 = new Firstprogram();
+    ob1.display();
+    ob1.display2();
+    }
+}
